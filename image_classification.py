2962cc1c5d26c84211e1ecb91da46f393d543a38
RexD <dongdi_unique@hotmail.com>
2017-06-08 21:34:18 +0800

    Add a simple binary model for pest classification.

--- image_classification.py
+++ image_classification.py
@@ -1,18 +1,24 @@
+import numpy as np
+
 from keras.applications import VGG16
 from keras import backend
 from keras.preprocessing import image
 from keras.applications import imagenet_utils as utils
-import numpy as np
 from keras.utils import plot_model
+from keras.models import load_model
 
 
 class Classifier(object):
 
     def __init__(self, model_type='vgg16'):
         # Instantiate VGG16 model with pre-trained weights from ImageNet
+        self.__model_type = model_type
         if model_type == 'vgg16':
             self.__model = VGG16(include_top=True, weights='imagenet')
-            self.__target_size = (224, 224)
+        elif model_type == 'pcm':
+            self.__conv_model = VGG16(include_top=False, weights='imagenet')
+            self.__fc_model = load_model('cm_pest')
+        self.__target_size = (224, 224)
 
     def __del__(self):
         backend.clear_session()
@@ -26,21 +32,29 @@ class Classifier(object):
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = utils.preprocess_input(x)
-        preds = utils.decode_predictions(self.__model.predict(x))
 
-        results = ''
-        for p in preds[0]:
-            results += '{}: {:.2f}%\n'.format(p[1], float(p[2] * 100))
+        if self.__model_type == 'vgg16':
+            preds = utils.decode_predictions(self.__model.predict(x))
+
+            results = ''
+            for p in preds[0]:
+                results += '{}: {:.2f}%\n'.format(p[1], float(p[2] * 100))
+        elif self.__model_type == 'pcm':
+            pred = self.__fc_model.predict(self.__conv_model.predict(x, batch_size=1), batch_size=1)
+            if int(pred) == 1:
+                results = 'mediterranean_fly'
+            else:
+                results = 'ambrosia_beetle'
         print(results)
         return results
 
 # test code
 if __name__ == "__main__":
-    classifier = Classifier()
-    # classifier.predict('dragonfly/dragonfly1.jpeg')
-    # classifier.predict('dragonfly/dragonfly2.jpeg')
-    # classifier.predict('dragonfly/dragonfly3.jpeg')
-    classifier.predict('granulate_ambrosia_beetle/0001.jpg')
-    classifier.predict('granulate_ambrosia_beetle/0002.jpg')
-    classifier.predict('granulate_ambrosia_beetle/0003.jpg')
-    classifier.predict('granulate_ambrosia_beetle/0005.jpg')
+    classifier = Classifier(model_type='vgg16')
+    classifier.predict('dragonfly/dragonfly1.jpeg')
+    classifier.predict('dragonfly/dragonfly2.jpeg')
+    classifier.predict('dragonfly/dragonfly3.jpeg')
+    # classifier.predict('data/train/mediterranean_fly/0001.jpg')
+    # classifier.predict('data/train/mediterranean_fly/0002.jpg')
+    # classifier.predict('data/train/mediterranean_fly/0003.jpg')
+    # classifier.predict('data/train/mediterranean_fly/0004.jpg')
--- train_model.py
+++ train_model.py
@@ -1,33 +1,87 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.applications import VGG16
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout
+
+
+def vgg_pp(x):
+    x = x[:, :, ::-1]
+    # Zero-center by mean pixel
+    x[:, :, 0] -= 103.939
+    x[:, :, 1] -= 116.779
+    x[:, :, 2] -= 123.68
+    return x
+
+batch_size = 17
+nb_train_samples = 136
+nb_validation_samples = 17
+
+
+model = VGG16(include_top=False, weights='imagenet')
+
+
 
 datagen = ImageDataGenerator(
-        rotation_range=90,
+        rotation_range=180,
         width_shift_range=0.1,
         height_shift_range=0.1,
-        zoom_range=0.2,
+        zoom_range=0.1,
         horizontal_flip=True,
         vertical_flip=True,
+        preprocessing_function=vgg_pp,
         fill_mode='nearest')
 
-batch_size = 16
-
-generator = datagen.flow_from_directory(
+train_generator = datagen.flow_from_directory(
         'data/train',
-        target_size=(150, 150),
+        target_size=(224, 224),
         batch_size=batch_size,
         class_mode=None,  # this means our generator will only yield batches of data, no labels
-        shuffle=True)
+        shuffle=False)
 
-img = load_img('data/train/granulate_ambrosia_beetle/0002.jpg')  # this is a PIL image
-x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
-x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
-print(x.shape)
+bottleneck_features_train = model.predict_generator(
+        train_generator, nb_train_samples // batch_size)
 
-# the .flow() command below generates batches of randomly transformed images
-# and saves the results to the `preview/` directory
-i = 0
-for batch in datagen.flow(x, batch_size=1,
-                          save_to_dir='data/preview', save_prefix='', save_format='jpeg'):
-    i += 1
-    if i > 20:
-        break  # otherwise the generator would loop indefinitely
+np.save('bottleneck_features_train', bottleneck_features_train)
+
+
+validation_generator = datagen.flow_from_directory(
+        'data/validation',
+        target_size=(224, 224),
+        batch_size=batch_size,
+        class_mode=None,  # this means our generator will only yield batches of data, no labels
+        shuffle=False)
+
+bottleneck_features_validation = model.predict_generator(
+        validation_generator, nb_validation_samples // batch_size)
+
+np.save('bottleneck_features_validation', bottleneck_features_validation)
+
+
+
+train_data = np.load('bottleneck_features_train.npy')
+train_labels = np.array(
+    [0] * (nb_train_samples // 2) + [1] * (nb_train_samples // 2))
+print(train_labels.shape)
+
+validation_data = np.load('bottleneck_features_validation.npy')
+validation_labels = np.array(
+    [0] * 9 + [1] * 8)
+print(validation_labels.shape)
+print(train_data.shape)
+
+model = Sequential()
+model.add(Flatten(input_shape=train_data.shape[1:]))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy', metrics=['accuracy'])
+
+model.fit(train_data, train_labels,
+          epochs=30,
+          batch_size=batch_size,
+          validation_data=(validation_data, validation_labels))
+## model.save_weights('top_fc_weights')
+model.save('cm_pest')
